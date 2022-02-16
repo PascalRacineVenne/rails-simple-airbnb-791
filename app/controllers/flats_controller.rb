f992dc57e5504759129445309a9f9852b5e0c8d9
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-02-16 16:50:54 -0500

    done for today

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -10,23 +10,25 @@ class FlatsController < ApplicationController
     @flat = Flat.new
   end
   
-  # def create
-  #   @flat = Flat.new(flat_params)
-  #   if @flat.save
-  #     redirect_to :
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
 
   def edit; end
 
   def update
-    
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
   end
 
   def destroy
-    
+    @flat.destroy
+    redirect_to flats_path
   end
 
   private
